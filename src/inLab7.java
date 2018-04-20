9c0973c238d0ff8eccdabcfc822fe20e6fbe338c
William <William@BEASTMACHINE>
2018-04-20 11:14:31 -0600

    Whitespace revisions and comment fixes.

--- src/inLab7.java
+++ src/inLab7.java
@@ -7,12 +7,12 @@
 public class inLab7{
     public static void main(String[] args) {
         int[][] arr = calulateReturn(new int[] {1,5,10,25},42);
-        int[] finalArr = new int[arr[arr.length-1][0]];
-        int farrindex=finalArr.length-1;
+        int[] finalArr = new int[arr[arr.length - 1][0]];
+        int farrindex = finalArr.length-1;
         int coinNum= arr[arr.length][1];
-        for (int i = arr.length-1; i>0;){
-            finalArr[farrindex]=arr[i][1];
-            i=i-arr[i][1];
+        for (int i = arr.length - 1; i > 0;){
+            finalArr[farrindex] = arr[i][1];
+            i = i-arr[i][1];
         }
         for ( int c: farrindex){
             System.out.println(c);
@@ -22,16 +22,16 @@ public class inLab7{
         if (coinTypes.length<1){System.out.println("ERROR coinTypes smaller than 1!"); return new int[][] {{-1}};}
         
         int[][] returnedCoins = new int[expectedChange][2];
-        returnedCoins[0]=new int[] {0,1};
-        returnedCoins[1]=new int[] {1,1};
+        returnedCoins[0] = new int[] {0,1};
+        returnedCoins[1] = new int[] {1,1};
         
         //int[] returnedCoins = {};
 
-        Dynamic Implementation
-        for (int c =0; c<returnedCoins.length;c++){
-            for(int v:coinTypes){
-                if (!(returnedCoins.length>v) && returnedCoins[c-v][0]>v){
-                    returnedCoins[c][0]= returnedCoins[c-v][0]+1; 
+        //Dynamic Implementation
+        for (int c = 0; c < returnedCoins.length; c++){
+            for(int v : coinTypes){
+                if (!(returnedCoins.length>v) && returnedCoins[c - v][0] > v){
+                    returnedCoins[c][0]= returnedCoins[c-v][0] + 1;
                 }
             }
         }
@@ -57,6 +57,5 @@ public class inLab7{
             }
         }*/
         return returnedCoins;
-        
     }
 }
