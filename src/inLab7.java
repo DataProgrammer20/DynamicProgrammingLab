535a3f28ff24f7886c643034840320427b976650
Allen Simpson <allen.simpson@motionencoding.com>
2018-04-20 00:23:06 -0600

    Uncommented and redid dynamic code

--- src/inLab7.java
+++ src/inLab7.java
@@ -6,23 +6,28 @@
 
 public class inLab7{
     public static void main(String[] args) {
-        int[] arr = calulateReturn(new int[] {1,5,10,25},42);
-        int c=0;
-        for (int v:arr) {
-            c++;
-            System.out.println(c+": "+v);
+        int[][] arr = calulateReturn(new int[] {1,5,10,25},42);
+        int[] finalArr = new int[arr[arr.length-1][0]];
+        int farrindex=finalArr.length-1;
+        int coinNum= arr[arr.length][1];
+        for (int i = arr.length-1; i>0;){
+            finalArr[farrindex]=arr[i][1];
+            i=i-arr[i][1];
+        }
+        for ( int c: farrindex){
+            System.out.println(c);
         }
     }
-    public static int[] calulateReturn (int[] coinTypes, int expectedChange){
-        if (coinTypes.length<1){System.out.println("ERROR coinTypes smaller than 1!"); return new int[] {-1};}
-        /*
+    public static int[][] calulateReturn (int[] coinTypes, int expectedChange){
+        if (coinTypes.length<1){System.out.println("ERROR coinTypes smaller than 1!"); return new int[][] {{-1}};}
+        
         int[][] returnedCoins = new int[expectedChange][2];
         returnedCoins[0]=new int[] {0,1};
         returnedCoins[1]=new int[] {1,1};
-        */
-        int[] returnedCoins = {};
+        
+        //int[] returnedCoins = {};
 
-        /* Dynamic Implementation
+        Dynamic Implementation
         for (int c =0; c<returnedCoins.length;c++){
             for(int v:coinTypes){
                 if (!(returnedCoins.length>v) && returnedCoins[c-v][0]>v){
@@ -30,7 +35,7 @@ public class inLab7{
                 }
             }
         }
-        */
+        /*
         int workingChange=expectedChange;
         int addedCoin;
         int remainderChange;
@@ -50,7 +55,8 @@ public class inLab7{
             for (int j=tempArr.length;j<returnedCoins.length;j++){
                 returnedCoins[j]=v;
             }
-        }
+        }*/
         return returnedCoins;
+        
     }
 }
