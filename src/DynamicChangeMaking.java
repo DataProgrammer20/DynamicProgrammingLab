f551b2bb45f2ac3239c7805558e1830795ab87aa
Allen Simpson <allen.simpson@motionencoding.com>
2018-04-21 02:27:41 -0600

    2:25 Late-Night Checkin, Refactoring Complete Bug Found testing on coinType Arrays not including 1. Scheduled to work at 1:00 PM On this.

--- src/DynamicChangeMaking.java
+++ src/DynamicChangeMaking.java
@@ -6,32 +6,64 @@
  * the method calculateReturn that finds the least-most number of coins to return as change.
  */
 public class DynamicChangeMaking {
-    public static int[] calculateReturn (int[] coinTypes, int expectedChange) {
+    public static void main(String[] args) {
+        int array[] = {1,5,10,25};
+        System.out.println ("\n-95-\n");
+        int[] rs = calculateReturn(array, 95);    
+        for (int r:rs){System.out.println (r);}
+        System.out.println ("\n-42-\n");
+        rs = calculateReturn(array, 42);    
+        for (int r:rs){System.out.println (r);}
+        System.out.println ("\n-103-\n");
+        rs = calculateReturn(array, 103); 
+        for (int r:rs){System.out.println (r);}
+        System.out.println ("\n-123-\n");   
+        rs = calculateReturn(array, 123); 
+        for (int r:rs){System.out.println (r);}
+        /////////////////////////////////////////////Tomorrow 1:00 Sharp
+        //////////////////*BUG*//////////////////////Errors when you are missing factors of the targetChange value 
+        /*
+        System.out.println ("\n-47 (new array)-\n");
+        array = new int[] {2,5,10,25};
+        rs = calculateReturn(array, 47);    
+        for (int r:rs){System.out.println (r);}
+        */
+    }
+
+    public static int[] calculateReturn (int[] coinTypes, int targetChange) {
         if (coinTypes.length == 0) {throw new IllegalArgumentException("Array of size zero is not allowed");}
-        if (expectedChange < 0) {throw new IllegalArgumentException("Value cannot be negative");}
-        if (expectedChange == 0) {return new int[]{0};}
-        int[][] returnedCoins = new int[expectedChange + 1][2];
-        returnedCoins[0] = new int[] {0,1};
-        returnedCoins[1] = new int[] {1,1};
-        int newCoin = 1;
-        // Dynamic Implementation
-        for (int c = 1; c < returnedCoins.length; c++) {
-            returnedCoins[c][0]=expectedChange;
-            for(int v : coinTypes) {
-                if (!((c-v) < 0) && returnedCoins[c-v][0] + 1 < returnedCoins[c][0]) {
-                    returnedCoins[c][0]=returnedCoins[c-v][0]+1;
-                    newCoin = v;
+        if (targetChange < 0) {throw new IllegalArgumentException("Value cannot be negative");}
+        if (targetChange == 0) {return new int[]{0};}
+        int[] coinNum = new int[targetChange + 1];
+        int[] lastCoinUsed = new int[targetChange + 1];
+        
+        //Setting terminal case where number of coins = 0
+        coinNum[0] = 0;
+        lastCoinUsed[0] = 0;
+        // Population of the pair of Data Arrays
+        for (int changeValue = 1; changeValue < coinNum.length; changeValue++) {                            //Interate over the data arrays
+            coinNum[changeValue] = Integer.MAX_VALUE;                                                       //Setting the coinNum changeValue to an upper-limit coinValue
+            for(int coinValue : coinTypes) {                                                                //Check different coin Types
+                int changeRemaining=changeValue-coinValue;                                                  //Caculate the remaining change
+                if (changeRemaining >= 0 &&                                                                 //*VALIDATION CHECK*//Change remaining must be positive,otherwise the value is not representable by a coin of that value (EX. 12 cannot be represented by a quarter)
+                    coinNum[changeRemaining] + 1 < coinNum[changeValue]) {                                  //   *LOGIC CHECK*  //Check that the value pointed to by the remaining change is better than the current value
+                    coinNum[changeValue]=coinNum[changeRemaining]+1;                                        //Assign coinNum changeValue to valid number given adding coin 'value'
+                    lastCoinUsed[changeValue]  = coinValue;                                                 
                 }
             }
-            returnedCoins[c][1] = newCoin;
         }
-        int[] finalArr = new int[returnedCoins[returnedCoins.length - 1][0]];
-        int farrindex = finalArr.length-1;
-        for (int i = returnedCoins.length-1; i>0;) {
-            finalArr[farrindex]=returnedCoins[i][1];
-            farrindex--;
-            i = i - returnedCoins[i][1];
+        // Parsing the pair of Data Arrays and populating the final return array
+        int[] returnedCoins = new int[coinNum[targetChange]];                                                    //Get number of coins in final answer
+        int i = 0;
+        while(lastCoinUsed[targetChange] > 0) {
+            returnedCoins[i] = lastCoinUsed[targetChange];
+            i++;
+            targetChange -= lastCoinUsed[targetChange];
         }
-        return finalArr;
+        return returnedCoins;
     }
 }
+//given that coinTypes = {1,5,10,25}
+//[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15]
+//  0  1  2  3  4  1  2  3  4  5  1  2  3  4  5  2
+//  1  1  1  1  1  5  1  1  1  1 10  1  1  1  1  5
